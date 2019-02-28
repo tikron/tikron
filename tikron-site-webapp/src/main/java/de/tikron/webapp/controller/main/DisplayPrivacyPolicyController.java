74af5975ad89b28b105663d564ce02cb862ca60a
Titus Kruse <mail@tituskruse.de>
2019-02-28 18:35:31 +0100

    Fix: robots.txt stimmte nicht mit meta Direktive überein.

---
+++ tikron-site-webapp/src/main/java/de/tikron/webapp/controller/main/DisplayPrivacyPolicyController.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2019 by Titus Kruse.
+ */
+package de.tikron.webapp.controller.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import de.tikron.webapp.controller.common.AbstractPageController;
+import de.tikron.webapp.util.RobotsDirective;
+
+/**
+ * Zeigt Informationen zum Datenschutz an.
+ * 
+ * @date 28.02.2019
+ * @author Titus Kruse
+ */
+@Controller
+@RequestMapping("/displayPrivacyPolicy.html")
+public class DisplayPrivacyPolicyController extends AbstractPageController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public void doGet() {
+	}
+
+	@Override
+	public String getRobotsDirective() {
+		return RobotsDirective.NOINDEX.toString();
+	}
+
+}
